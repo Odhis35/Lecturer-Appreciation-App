df1afc67e90e6db536f509c84364e172897bf5e3
Odhis35 <donodhiambo.93@gmail.com>
2026-02-20 01:35:45 +0300

    Updated homepage and added Javascript file

---
+++ app.js
@@ -0,0 +1,6 @@
+let inputEl = document.getElementById("input_el")
+let messageEl = document.getElementById("message_el")
+function myMessage(){
+    let message = inputEl.value
+    messageEl.innerText = message
+}
