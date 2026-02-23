472d02e0fb293019aa9473e181bb0b8a78913530
Odhis35 <donodhiambo.93@gmail.com>
2026-02-24 01:49:54 +0300

    Add app.css

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-let inputEl = document.getElementById("input_el")
-let messageEl = document.getElementById("message_el")
-function myMessage(){
-    let message = inputEl.value
-    messageEl.innerText = message
-}  
+//let inputEl = document.getElementById("input_el")
+//let messageEl = document.getElementById("message_el")
+//function myMessage(){
+    //let message = inputEl.value
+    //messageEl.innerText = message
+//}  
+function sendMessage(user){
+    let inputField
+    if(user === 'User1'){
+        inputField = document.getElementById("input-1");
+    } else inputField = document.getElementById("input-2");
+}
+
+let messageText = inputField.value;
+if (messageText.trim() === " ") return;
+//create an element
+let message = document.createElement("div")
+message.classList.add("message", user)
+message.textContent = messageText;
+
+//append to chatbox
+document.getElementById("chatbox").appendChild(message)
+
+//clear inputfield
+inputField.value = " ";
